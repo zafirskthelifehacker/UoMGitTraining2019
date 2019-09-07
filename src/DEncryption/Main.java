c8e65ba70723665f684460bd8c186ece5566b6e3
Dharmesh-Sookun <sookundharmesh2906@gmail.com>
2019-09-07 19:19:01 +0400

    Added comments

--- src/DEncryption/Main.java
+++ src/DEncryption/Main.java
@@ -6,44 +6,48 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Input Text: ");
-		String text = input.next();
-		StringBuffer encryptedText = Encrypt(text);
-		System.out.println("Encrypted Text is " + encryptedText);
-		StringBuffer decryptedText = Decrypt(encryptedText.toString());
-		System.out.println("Decrypted Text is " + decryptedText);
+		String text = input.next(); // Takes the user's input
+		StringBuffer encryptedText = Encrypt(text); // Passes the user's text into the Encrypt() method 
+		System.out.println("Encrypted Text is " + encryptedText); // Displaying the encrypted text
+		StringBuffer decryptedText = Decrypt(encryptedText.toString()); // Passes the encrypted text into the Decrypt() method
+		System.out.println("Decrypted Text is " + decryptedText); // Displaying the decrypted text
 	}
+	
+	//The Encrypt() method starts here.
 	public static StringBuffer Encrypt(String text){ 
-		StringBuffer encryptionResult= new StringBuffer(); 
-		for (int i = 0; i < text.length(); i++) 
+		StringBuffer encryptionResult= new StringBuffer(); // Creating an object of the StringBuffer class
+		for (int i = 0; i < text.length(); i++) // Looping through each character of the text
 		{ 
 		    if (Character.isUpperCase(text.charAt(i))) 
 		    { 
-		        char ch = (char)(((int)text.charAt(i) + 4 - 65) % 26 + 65); 
-		        encryptionResult.append(ch); 
+		        char ch = (char)(((int)text.charAt(i) + 4 - 65) % 26 + 65); //Encrypting the upper case characters
+		        encryptionResult.append(ch); // Adding each result character to StringBuffer object
 		    } 
 		    else
 		    { 
-		        char ch = (char)(((int)text.charAt(i) + 4 - 97) % 26 + 97); 
-		        encryptionResult.append(ch); 
+		        char ch = (char)(((int)text.charAt(i) + 4 - 97) % 26 + 97); //Encrypting the lower case characters
+		        encryptionResult.append(ch); // Adding each result character to StringBuffer object
 		    } 
 		} 
-		return encryptionResult; 
+		return encryptionResult; // Returning the object
 	} 
+	
+	// The method Decrypt() starts here.
 	public static StringBuffer Decrypt(String text){ 
-		StringBuffer decryptionResult = new StringBuffer(); 
-		for (int i = 0; i < text.length(); i++) 
+		StringBuffer decryptionResult = new StringBuffer(); // Creating an object of the StringBuffer class
+		for (int i = 0; i < text.length(); i++) // Looping through each character of the text
 		{ 
 		    if (Character.isUpperCase(text.charAt(i))) 
 		    { 
-		    	char ch = (char)(((int)text.charAt(i) - (4 + 65)) % 26 + 65);  
-		        decryptionResult.append(ch); 
+		    	char ch = (char)(((int)text.charAt(i) - (4 + 65)) % 26 + 65);  //Decrypting the upper case characters
+		        decryptionResult.append(ch); // Adding each result character to StringBuffer object
 		    } 
 		    else
 		    { 
-		    	char ch = (char)(((int)text.charAt(i) - (4 + 97)) % 26 + 97);  
-		        decryptionResult.append(ch); 
+		    	char ch = (char)(((int)text.charAt(i) - (4 + 97)) % 26 + 97);  //Decrypting the lower case characters
+		        decryptionResult.append(ch); // Adding each result character to StringBuffer object
 		    } 
 		} 
-		return decryptionResult; 
+		return decryptionResult; // Returning the object
 	} 
 }
